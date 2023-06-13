7d45d7a0acf9a87fa76b5bd1173def3c294e967b
Shyam Sunder Kanth <shyamkanth0805@gmail.com>
2023-06-13 16:13:41 +0530

    Update Main.java

---
+++ src/main/java/Main.java
@@ -0,0 +1,34 @@
+package org.example;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String[] menu = {
+                "1. Pizza",
+                "2. Burger",
+                "3. Salad",
+                "4. Pasta",
+                "5. Sandwich"
+        };
+
+        System.out.println("Menu:");
+        for (String item : menu) {
+            System.out.println(item);
+        }
+
+        System.out.print("Enter your choice (1-5): ");
+        Scanner scanner = new Scanner(System.in);
+        int choice = scanner.nextInt();
+
+        if (choice >= 1 && choice <= 5) {
+            String foodItem = menu[choice - 1].substring(3);
+            System.out.println("Your order of " + foodItem + " has been accepted!");
+        } else {
+            System.out.println("Invalid choice. Please try again.");
+        }
+
+        scanner.close();
+    }
+}
+
