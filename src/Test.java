f417e35b08e568ae0cb19adead0c4ad50b6b2c8f
Shyam Sunder Kanth <shyamkanth0805@gmail.com>
2023-06-14 14:06:29 +0530

    Test updated

--- src/Test.java
+++ src/Test.java
@@ -1,19 +1,43 @@
 import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Test {
     public static void main(String[] args) {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String line = null;
+        String csvFile = "food.csv";
+        String line;
+        String csvSplitBy = ",";
+        boolean found = false;
 
-        try {
-            System.out.print("Enter a line: ");
-            line = reader.readLine();
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            System.out.print("Enter a food item: ");
+            String searchItem = reader.readLine();
+
+            try (BufferedReader fileReader = new BufferedReader(new FileReader(csvFile))) {
+                // Skip the header row
+                fileReader.readLine();
+
+                while ((line = fileReader.readLine()) != null) {
+                    String[] foodData = line.split(csvSplitBy);
+                    String foodItem = foodData[0];
+
+                    if (foodItem.equalsIgnoreCase(searchItem)) {
+                        String price = foodData[1];
+                        System.out.println("Your order for \"" + foodItem + "\" has been accepted, and the cost is " + price + " Rupees.");
+                        found = true;
+                        break;
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            if (!found) {
+                System.out.println("This item is not available currently.");
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        System.out.println("You entered: " + line);
     }
 }
